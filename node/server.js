bb498fa5bb8dab2a11e3a3ffab43cc3ab8d1428f
Stefan Dimitrov <sdimitrov@fliptrx.com>
2021-10-11 11:38:36 -0400

    manually inject headers

--- node/server.js
+++ node/server.js
@@ -3,7 +3,6 @@ tracer.init({
   logInjection: true
 });
 tracer.use('http', { splitByDomain: true });
-import { FORMAT_HTTP_HEADERS } from 'opentracing'
 import express from 'express';
 import invoke from './service.js';
 
@@ -12,11 +11,7 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   console.log('DD POC - Node\n');
-  const span = tracer.startSpan("span");
-  const headersObj = {};
-  tracer.inject(span, FORMAT_HTTP_HEADERS, headersObj);
   invoke(res);
-  console.log(headersObj);
 });
 
 app.listen(port, () => {
