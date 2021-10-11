bb0900eb31955f428a00a4d0f02ecaa9351590aa
Stefan Dimitrov <sdimitrov@fliptrx.com>
2021-10-11 13:34:46 -0400

    switch to json logging

--- flask/main.py
+++ flask/main.py
@@ -3,14 +3,18 @@ from flask import Flask, request
 import os
 import json
 import logging
+from pythonjsonlogger import jsonlogger
 from ddtrace import tracer
 
 FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
           '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
           '- %(message)s')
-logging.basicConfig(format=FORMAT)
-log = logging.getLogger(__name__)
+logger = logging.getLogger()
+logHandler = logging.StreamHandler()
+formatter = jsonlogger.JsonFormatter()
 log.level = logging.DEBUG
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
 
 tracer.configure(
     hostname=os.environ.get('DD_AGENT_HOST'),
