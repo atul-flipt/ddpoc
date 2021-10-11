caf80fa6d5897c180fb6058858d6c3a2dcba488d
Stefan Dimitrov <sdimitrov@fliptrx.com>
2021-10-11 09:27:24 -0400

    switch to http

--- node/service.js
+++ node/service.js
@@ -1,16 +1,24 @@
 
 
-import request from 'request'
+import * as http from 'http';
 
-
-
-export default function invoke(res) {
+export default function invoke(r) {
   console.log("Calling flask @ localhost:5010")
-  request('http://localhost:5010', { json: true }, (err, r, body) => {
-    if (err) { return console.log(err); }
-    console.log(body.url);
-    console.log(body.data);
-    res.send('Hello World!');
+  http.get({
+    host: 'localhost',
+    port: 5010,
+    path: '/'
+  }, function (res) {
+    var body = '';
+    res.on('data', function (chunk) {
+      body += chunk;
+    });
+    res.on('end', function () {
+      console.log(body);
+      r.send(body);
+    });
+  }).on('error', function (e) {
+    console.log("Got error: " + e.message);
   });
 
 
