9a5e4075b9355a52c655d3e20f6220c492a63590
Stefan Dimitrov <sdimitrov@fliptrx.com>
2021-10-11 12:52:09 -0400

    add node logger

---
+++ node/logger.js
@@ -0,0 +1,6 @@
+import winston from 'winston';
+
+export default winston.createLogger({
+  level: 'debug',
+  transports: new winston.transports.Console()
+});
--- node/server.js
+++ node/server.js
@@ -1,13 +1,25 @@
+import logger from './logger.js'
 import tracer from 'dd-trace';
 tracer.init({
-  logInjection: true
+  logInjection: true,
+  logger
 });
 tracer.use('http', { splitByDomain: true });
 import express from 'express';
+import expressWinston from 'express-winston';
 import invoke from './service.js';
 
 const app = express();
 const port = 3000;
+
+app.use(expressWinston.logger({
+  winstonInstance: logger
+}));
+
+app.use(expressWinston.errorLogger({
+  winstonInstance: logger
+}))
+
 
 app.get('/', (req, res) => {
   console.log('DD POC - Node\n');
