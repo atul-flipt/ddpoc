e0322379c701f9d3f6c0b19cb1eae714feb8abf0
Stefan Dimitrov <sdimitrov@fliptrx.com>
2021-10-11 12:59:35 -0400

    fix node logging

--- node/server.js
+++ node/server.js
@@ -5,12 +5,42 @@ tracer.init({
   logger
 });
 tracer.use('http', { splitByDomain: true });
+import http from 'http';
 import express from 'express';
 import expressWinston from 'express-winston';
-import invoke from './service.js';
 
 const app = express();
 const port = 3000;
+
+const invoke = function invoke(r) {
+  logger.log({
+    level: 'info',
+    message: "Calling flask @ dockerhost:5010"
+  });
+  http.get({
+    host: 'dockerhost',
+    port: 5010,
+    path: '/',
+    headers
+  }, function (res) {
+    var body = '';
+    res.on('data', function (chunk) {
+      body += chunk;
+    });
+    res.on('end', function () {
+      logger.log({
+        level: 'info',
+        message: body
+      });
+      r.send(body);
+    });
+  }).on('error', function (e) {
+    logger.log({
+      level: 'error',
+      message: e.message
+    });
+  });
+}
 
 app.use(expressWinston.logger({
   winstonInstance: logger
@@ -19,7 +49,6 @@ app.use(expressWinston.logger({
 app.use(expressWinston.errorLogger({
   winstonInstance: logger
 }))
-
 
 app.get('/', (req, res) => {
   console.log('DD POC - Node\n');
