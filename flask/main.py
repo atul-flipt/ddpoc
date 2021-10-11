53956ec4e4e2384286045b1a3af16ce2e3ad14e0
Stefan Dimitrov <sdimitrov@fliptrx.com>
2021-10-11 13:42:18 -0400

    fix logging

--- flask/main.py
+++ flask/main.py
@@ -9,12 +9,12 @@ from ddtrace import tracer
 FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] '
           '[dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] '
           '- %(message)s')
-logger = logging.getLogger()
-logHandler = logging.StreamHandler()
-formatter = jsonlogger.JsonFormatter()
-log.level = logging.DEBUG
-logHandler.setFormatter(formatter)
-logger.addHandler(logHandler)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+json_handler = logging.StreamHandler()
+formatter = jsonlogger.JsonFormatter(fmt='%(asctime)s %(levelname)s %(name)s %(dd.service)s %(dd.env)s %(dd.version)s %(dd.trace_id)s %(dd.span_id)s %(message)s')
+json_handler.setFormatter(formatter)
+logger.addHandler(json_handler)
 
 tracer.configure(
     hostname=os.environ.get('DD_AGENT_HOST'),
