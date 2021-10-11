4da77f75360d8c5634d195c93f5dfcd4e6f08f5d
Stefan Dimitrov <sdimitrov@fliptrx.com>
2021-10-11 11:38:51 -0400

    manually inject headers

--- node/service.js
+++ node/service.js
@@ -1,13 +1,17 @@
-
-
+import tracer from 'dd-trace';
+import { FORMAT_HTTP_HEADERS } from 'opentracing'
 import * as http from 'http';
 
 export default function invoke(r) {
-  console.log("Calling flask @ localhost:5010")
+  console.log("Calling flask @ localhost:5010");
+  const span = tracer.startSpan("span");
+  const headers = {};
+  tracer.inject(span, FORMAT_HTTP_HEADERS, headers);
   http.get({
     host: 'localhost',
     port: 5010,
-    path: '/'
+    path: '/',
+    headers
   }, function (res) {
     var body = '';
     res.on('data', function (chunk) {
